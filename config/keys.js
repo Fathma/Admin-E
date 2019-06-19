681f460a6350e752c140e07ffc24bdc26d310346
Fathma <siddiquefathma@gmail.com>
2019-06-19 16:34:00 +0600

    make it better

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+    database:{
+        mongoURI: "mongodb://jihad:abc1234@ds343985.mlab.com:43985/e-commerce_db_v1"
+    },
+    email:{
+        MAILGUN_USER: 'devtestjihad@gmail.com',
+        MAILGUN_PASS: 'DevTestJihad#1234'
+    }
+}
